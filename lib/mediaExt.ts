fe9e7eb7f03c54fd37e8432efcdaeac95620a817
KR Shanto <krshanto2005@gmail.com>
2023-05-01 23:40:03 +0600

    Create pic, vid and audio file types

---
+++ lib/mediaExt.ts
@@ -0,0 +1,23 @@
+export const pictureExt = [
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "image/webp",
+  "image/gif",
+];
+
+export const videoExt = [
+  "video/mp4",
+  "video/mpeg",
+  "video/ogg",
+  "video/webm",
+  "video/3gpp",
+];
+
+export const audioExt = [
+  "audio/mpeg",
+  "audio/ogg",
+  "audio/wav",
+  "audio/webm",
+  "audio/mp3",
+];
