c3868e6d7d576b638424df29725349aea310d570
KR Shanto <krshanto2005@gmail.com>
2023-05-01 23:38:45 +0600

    Allow firebase domain in next config to serve image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  images: {
+    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
