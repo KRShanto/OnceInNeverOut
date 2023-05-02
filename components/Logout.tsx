f86023665304a1364fdb64f93c00adae9b45cc6c
KR Shanto <krshanto2005@gmail.com>
2023-05-02 17:24:13 +0600

    Improve logout component

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { signOut } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import Link from "next/link";
 
 export default function Logout() {
-  return <div>Logout</div>;
+  const router = useRouter();
+  const { prev } = router.query as { prev: string };
+
+  return (
+    <div className="logout">
+      <h3 className="title">Are you sure you want to logout?</h3>
+
+      <div className="options">
+        <button
+          className="option danger"
+          onClick={() => {
+            signOut(auth);
+            router.push("/");
+          }}
+        >
+          Yes
+        </button>
+
+        <Link
+          className="option cancel
+        "
+          href={prev || "/"}
+        >
+          No
+        </Link>
+      </div>
+    </div>
+  );
 }
